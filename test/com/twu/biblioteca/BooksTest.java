5797dbc3fe56eef6077f8b47df4582465b3194a3
maharjun <maharjun@thoughtworks.com>
2015-09-05 10:19:53 +0530

    Added spec for getting all books which are presents in book list

--- test/com/twu/biblioteca/BooksTest.java
+++ test/com/twu/biblioteca/BooksTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 public class BooksTest {
 
     @Test
-    public void shouldPrintTheAllBooks() {
+    public void shouldPrintTheOneBook() {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(byteArrayOutputStream));
         List<Book> list = new ArrayList<>();
@@ -22,5 +22,19 @@ public class BooksTest {
         books.printBooksList();
 
         assertEquals("Head First Java\tKathy Sierra\t2009\n",byteArrayOutputStream.toString());
+    }
+
+    @Test
+    public void shouldPrintTheAllBooks() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        List<Book> list = new ArrayList<>();
+        list.add(new Book("Head First Java", "Kathy Sierra", 2009));
+        list.add(new Book("Pragmattic Programmer", "Andrew Hunt", 2009));
+        list.add(new Book("Martin's Refactoring", "Martin Fowler", 2009));
+        Books books = new Books(list);
+        books.printBooksList();
+
+        assertEquals("Head First Java\tKathy Sierra\t2009\nPragmattic Programmer\tAndrew Hunt\t2009\nMartin's Refactoring\tMartin Fowler\t2009\n",byteArrayOutputStream.toString());
     }
 }
