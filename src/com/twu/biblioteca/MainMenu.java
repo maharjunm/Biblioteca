1d2964079434c2d1b0c1b18cdfaae11987147799
maharjun <maharjun@thoughtworks.com>
2015-09-05 12:59:49 +0530

    Refoctored MainMenuTest class for Added duplicate code into one method

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -2,10 +2,24 @@ package com.twu.biblioteca;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainMenu {
     public void printOptions() {
         System.out.println("1.List Books");
         System.out.print("Choose one Option :");
+        Scanner scanner = new Scanner(System.in);
+        String option=scanner.nextLine();
+
+        processUserOption(option);
+    }
+
+    private void processUserOption(String option) {
+        if(option.equals("1")){
+            List<Book> list = new ArrayList<>();
+            list.add(new Book("Head First Java", "Kathy Sierra", 2009));
+            Books books = new Books(list);
+            books.printBooks();
+        }
     }
 }
--- test/com/twu/biblioteca/MainMenuTest.java
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -11,20 +12,42 @@ public class MainMenuTest {
     @Test
     public void shouldPrintTheMenuOptions() {
         MainMenu mainMenu = new MainMenu();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(byteArrayOutputStream));
+        ByteArrayOutputStream byteArrayOutputStream = getByteArrayInputAndOutputStream();
         mainMenu.printOptions();
 
-        Assert.assertEquals("1.List Books\nChoose one Option :",byteArrayOutputStream.toString());
+        Assert.assertEquals("1.List Books\nChoose one Option :Book Name\tAuthour Name\tYear Published\n" +
+                "Head First Java\tKathy Sierra\t2009\n", byteArrayOutputStream.toString());
     }
 
     @Test
     public void shouldPrintTheOptionAndChooseOptionAlso() {
         MainMenu mainMenu = new MainMenu();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(byteArrayOutputStream));
+        ByteArrayOutputStream byteArrayOutputStream = getByteArrayInputAndOutputStream();
+
         mainMenu.printOptions();
 
-        Assert.assertEquals("1.List Books\nChoose one Option :",byteArrayOutputStream.toString());
+        Assert.assertEquals("1.List Books\nChoose one Option :Book Name\tAuthour Name\tYear Published\n" +
+                "Head First Java\tKathy Sierra\t2009\n", byteArrayOutputStream.toString());
+    }
+
+    @Test
+    public void shouldPrintTheOptionAndChooseOptionIfHeChoose1ThenPrintTheListOfBooks() {
+        MainMenu mainMenu = new MainMenu();
+        ByteArrayOutputStream byteArrayOutputStream = getByteArrayInputAndOutputStream();
+
+        mainMenu.printOptions();
+
+        Assert.assertEquals("1.List Books\nChoose one Option :Book Name\tAuthour Name\tYear Published\n" +
+                "Head First Java\tKathy Sierra\t2009\n", byteArrayOutputStream.toString());
+    }
+
+    private ByteArrayOutputStream getByteArrayInputAndOutputStream() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        String input = "1";
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+        return byteArrayOutputStream;
     }
 }
