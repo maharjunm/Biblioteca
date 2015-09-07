b9db700538e356020377abdf06df1defc3621c29
maharjun <maharjun@thoughtworks.com>
2015-09-07 12:15:55 +0530

    Added spec for printing welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,4 +5,15 @@ import java.util.List;
 
 public class BibliotecaApp {
 
+    public static void main(String[] args) {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+    }
+
+    public void start() {
+        printWelcomeMessage("Welcome To Biblioteca");
+    }
+
+    private void printWelcomeMessage(String welcomeMessage) {
+        System.out.println(welcomeMessage);
+    }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,10 +3,20 @@ package com.twu.biblioteca;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
 public class BibliotecaAppTest {
 
     @Test
     public void shouldPrintTheWelcomeMessage() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        bibliotecaApp.start();
 
+        assertEquals("Welcome To Biblioteca\n",outContent.toString());
     }
 }
