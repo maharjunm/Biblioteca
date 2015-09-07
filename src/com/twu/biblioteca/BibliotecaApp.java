b01e42c5054491bec59b017b76e47c3f00c23814
maharjun <maharjun@thoughtworks.com>
2015-09-07 12:07:01 +0530

    Starting From the Scratch due to bad design-- this is intial commit with directory structure

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,20 +5,4 @@ import java.util.List;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.start();
-    }
-
-    public void start() {
-        printWelcomeMessage();
-        List<Book> list = new ArrayList<>();
-        list.add(new Book("Head First Java", "Kathy Sierra", 2009));
-        Books books = new Books(list);
-        books.printBooks();
-    }
-
-    public void printWelcomeMessage() {
-        System.out.println("Welcome to Biblioteca");
-    }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,38 +3,10 @@ package com.twu.biblioteca;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 public class BibliotecaAppTest {
 
     @Test
     public void shouldPrintTheWelcomeMessage() {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(byteArrayOutputStream));
-        bibliotecaApp.printWelcomeMessage();
 
-        Assert.assertEquals("Welcome to Biblioteca\n", byteArrayOutputStream.toString());
-    }
-
-    @Test
-    public void shouldPrintTheWelcomeMessageWhenWeStartTheApplication() {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(byteArrayOutputStream));
-        bibliotecaApp.start();
-
-        Assert.assertEquals("Welcome to Biblioteca\n", byteArrayOutputStream.toString());
-    }
-
-    @Test
-    public void shouldPrintTheWelcomeMessageAndListOfBooksWhenWeStartTheApplication() {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(byteArrayOutputStream));
-        bibliotecaApp.start();
-
-        Assert.assertEquals("Welcome to Biblioteca\nHead First Java\nPragmatic Programming\nMartin's Refactoring\n", byteArrayOutputStream.toString());
     }
 }
