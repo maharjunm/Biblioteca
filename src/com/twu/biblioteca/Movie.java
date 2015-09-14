8b1439e654443d3cb2afdb614b39410a6d79cec4
maharjun <maharjun@thoughtworks.com>
2015-09-14 15:30:21 +0530

    Added spec to movie test should print the movie name , year it released and director of the movie

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -3,16 +3,18 @@ package com.twu.biblioteca;
 
 public class Movie {
 
+    private String director;
     private int yearReleased;
     private String movieName;
 
-    public Movie(String movieName, int yearReleased) {
+    public Movie(String movieName, int yearReleased, String director) {
         this.movieName = movieName;
         this.yearReleased = yearReleased;
+        this.director = director;
     }
 
     @Override
     public String toString() {
-        return movieName + "\t" + yearReleased;
+        return movieName + "\t" + yearReleased + "\t" + director;
     }
 }
--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -8,15 +8,22 @@ public class MovieTest {
 
     @Test
     public void shouldReturnTheMovieName() {
-        Movie movie = new Movie("3 Idiots", 2009);
+        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani");
 
-        assertEquals("3 Idiots\t2009", movie.toString());
+        assertEquals("3 Idiots\t2009\tRajkumar Hirani", movie.toString());
     }
 
     @Test
     public void shouldReturnTheMovieNameAndYear() {
-        Movie movie = new Movie("3 Idiots", 2009);
+        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani");
 
-        assertEquals("3 Idiots\t2009", movie.toString());
+        assertEquals("3 Idiots\t2009\tRajkumar Hirani", movie.toString());
+    }
+
+    @Test
+    public void shouldReturnTheMovieNameYearAndDirectorName() {
+        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani");
+
+        assertEquals("3 Idiots\t2009\tRajkumar Hirani", movie.toString());
     }
 }
