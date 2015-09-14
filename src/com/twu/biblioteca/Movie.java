4197ee6f7d46dbd9b10d0c5f9c519479be150ff0
maharjun <maharjun@thoughtworks.com>
2015-09-14 15:46:01 +0530

    Added spec to movie test to print the rating of movie along with name, year and director

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -3,18 +3,20 @@ package com.twu.biblioteca;
 
 public class Movie {
 
+    private double ratingOfTheMovie;
     private String director;
     private int yearReleased;
     private String movieName;
 
-    public Movie(String movieName, int yearReleased, String director) {
+    public Movie(String movieName, int yearReleased, String director, double ratingOfTheMovie) {
         this.movieName = movieName;
         this.yearReleased = yearReleased;
         this.director = director;
+        this.ratingOfTheMovie = ratingOfTheMovie;
     }
 
     @Override
     public String toString() {
-        return movieName + "\t" + yearReleased + "\t" + director;
+        return movieName + "\t" + yearReleased + "\t" + director + "\t" + ratingOfTheMovie;
     }
 }
--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -8,22 +8,29 @@ public class MovieTest {
 
     @Test
     public void shouldReturnTheMovieName() {
-        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani");
+        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani", 8.5);
 
-        assertEquals("3 Idiots\t2009\tRajkumar Hirani", movie.toString());
+        assertEquals("3 Idiots\t2009\tRajkumar Hirani\t8.5", movie.toString());
     }
 
     @Test
     public void shouldReturnTheMovieNameAndYear() {
-        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani");
+        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani", 8.5);
 
-        assertEquals("3 Idiots\t2009\tRajkumar Hirani", movie.toString());
+        assertEquals("3 Idiots\t2009\tRajkumar Hirani\t8.5", movie.toString());
     }
 
     @Test
     public void shouldReturnTheMovieNameYearAndDirectorName() {
-        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani");
+        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani", 8.5);
 
-        assertEquals("3 Idiots\t2009\tRajkumar Hirani", movie.toString());
+        assertEquals("3 Idiots\t2009\tRajkumar Hirani\t8.5", movie.toString());
+    }
+
+    @Test
+    public void shouldReturnTheMovieNameYearDirectorNameAndRateOfTheMovie() {
+        Movie movie = new Movie("3 Idiots", 2009, "Rajkumar Hirani" , 8.5);
+
+        assertEquals("3 Idiots\t2009\tRajkumar Hirani\t8.5", movie.toString());
     }
 }
