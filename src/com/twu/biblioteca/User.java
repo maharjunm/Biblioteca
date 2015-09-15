ffdde5c53fb1d99dea5b0f933bebb5855993912a
maharjun <maharjun@thoughtworks.com>
2015-09-15 12:20:08 +0530

    added spc to user test should return the email address along with the name of the user

--- src/com/twu/biblioteca/User.java
+++ src/com/twu/biblioteca/User.java
@@ -2,14 +2,16 @@
 package com.twu.biblioteca;
 
 public class User {
+    private String email;
     private String name;
 
-    public User(String name) {
+    public User(String name, String email) {
         this.name = name;
+        this.email = email;
     }
 
     @Override
     public String toString() {
-        return name;
+        return name + "\t" + email;
     }
 }
--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -7,8 +7,15 @@ public class UserTest {
 
     @Test
     public void shouldReturnTheUserName() {
-        User user = new User("Maharjun");
+        User user = new User("Maharjun", "maharjun@thoughtworks.com");
 
         Assert.assertEquals("Maharjun",user.toString());
+    }
+
+    @Test
+    public void shouldReturnTheEmailAddress() {
+        User user = new User("Maharjun","maharjun@thoughtworks.com");
+
+        Assert.assertEquals("Maharjun\tmaharjun@thoughtworks.com",user.toString());
     }
 }
