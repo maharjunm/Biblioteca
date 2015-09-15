3514e65a4dd539cde272c6427d77a52b5d288ab1
maharjun <maharjun@thoughtworks.com>
2015-09-15 11:29:01 +0530

    modified spec in ment test should show the checkout movie option in the starting menu and modifications done in specs in menu test to show the options

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -2,7 +2,7 @@
 package com.twu.biblioteca;
 
 public class Menu {
-    private String[] options = {"List Books", "List Movies", "Quit"};
+    private String[] options = {"List Books", "List Movies", "Checkout Movie", "Quit"};
 
     @Override
     public String toString() {
