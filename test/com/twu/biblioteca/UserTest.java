3ebd92010dba01cbe29a0b13ca4e3513c61a4319
maharjun <maharjun@thoughtworks.com>
2015-09-15 14:07:49 +0530

    added spec in user test should return false when login passwords are not same and deleted prevous specs

--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -8,9 +8,16 @@ import static org.junit.Assert.*;
 public class UserTest {
 
     @Test
-    public void shouldReturnFalseWhenUserIsNotEquals() {
+    public void shouldReturnFalseWhenUserNameIsNotEquals() {
         User user = new User("B09-1893","B091893");
 
         assertFalse(user.compare("maharjun","B091893"));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenPasswordIsNotEquals() {
+        User user = new User("B09-1893","B091893");
+
+        assertFalse(user.compare("B09-1893","maharjun"));
     }
 }
