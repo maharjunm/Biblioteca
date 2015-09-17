fa6ebc084cb97129d7e744b1e2d9bf3de0b123c4
maharjun <maharjun@thoughtworks.com>
2015-09-17 15:32:21 +0530

    User should return the phone number of the user along with name and email id

--- src/com/twu/biblioteca/User.java
+++ src/com/twu/biblioteca/User.java
@@ -3,18 +3,20 @@ package com.twu.biblioteca;
 
 public class User {
 
+    private String phoneNumber;
     private String email;
     private String name;
     private String password;
     private String loginId;
     private String role;
 
-    public User(String loginId, String password, String role, String name, String email) {
+    public User(String loginId, String password, String role, String name, String email, String phoneNumber) {
         this.loginId = loginId;
         this.password = password;
         this.role = role;
         this.name = name;
         this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
     public boolean compare(String loginId, String password) {
@@ -28,7 +30,7 @@ public class User {
     }
 
     @Override
-    public String toString(){
-        return name + "\t" + email;
+    public String toString() {
+        return name + "\t" + email + "\t" + phoneNumber;
     }
 }
--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -8,43 +8,50 @@ public class UserTest {
 
     @Test
     public void shouldReturnFalseWhenUserNameIsNotEquals() {
-        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com");
+        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com", "8498984842");
 
         assertFalse(user.compare("maharjun", "B091893"));
     }
 
     @Test
     public void shouldReturnFalseWhenPasswordIsNotEquals() {
-        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com");
+        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com", "8498984842");
 
         assertFalse(user.compare("B09-1893", "maharjun"));
     }
 
     @Test
     public void shouldReturnTrueWhenBothEquals() {
-        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com");
+        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com", "8498984842");
 
         assertTrue(user.compare("B09-1893", "B091893"));
     }
 
     @Test
     public void shouldReturnTheRoleOfTheUser() {
-        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com");
+        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com", "8498984842");
 
         assertEquals("Librarian", user.getRole());
     }
 
     @Test
     public void shouldReturnNameOfTheUser() {
-        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com");
+        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com", "8498984842");
 
-        assertEquals("Maharjun", user.toString());
+        assertEquals("Maharjun\tmaharjun123@gmail.com\t8498984842", user.toString());
     }
 
     @Test
     public void shouldReturnEmailOfTheUser() {
-        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com");
+        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com", "8498984842");
 
-        assertEquals("Maharjun\tmaharjun123@gmail.com", user.toString());
+        assertEquals("Maharjun\tmaharjun123@gmail.com\t8498984842", user.toString());
+    }
+
+    @Test
+    public void shouldReturnPhoneNumberOfTheUser() {
+        User user = new User("B09-1893", "B091893", "Librarian", "Maharjun", "maharjun123@gmail.com", "8498984842");
+
+        assertEquals("Maharjun\tmaharjun123@gmail.com\t8498984842", user.toString());
     }
 }
