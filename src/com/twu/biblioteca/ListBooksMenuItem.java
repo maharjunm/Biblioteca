c04f99c033849d81a569cb63893a03fe1bc0c82c
maharjun <maharjun@thoughtworks.com>
2015-09-24 23:01:39 +0530

    List books menu item should return the list books

--- src/com/twu/biblioteca/ListBooksMenuItem.java
+++ src/com/twu/biblioteca/ListBooksMenuItem.java
@@ -1,7 +1,18 @@
 package com.twu.biblioteca;
 
 public class ListBooksMenuItem {
+
+    private Library library;
+
+    public ListBooksMenuItem(Library library) {
+        this.library = library;
+    }
+
     public String option() {
         return "List Books";
+    }
+
+    public void execute() {
+        library.listBooks();
     }
 }
--- test/com/twu/biblioteca/ListBooksMenuItemTest.java
+++ test/com/twu/biblioteca/ListBooksMenuItemTest.java
@@ -3,13 +3,27 @@ package com.twu.biblioteca;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class ListBooksMenuItemTest {
 
     @Test
     public void shouldReturnTheListBooksOption() {
-        ListBooksMenuItem menuItem = new ListBooksMenuItem();
+        Library library = mock(Library.class);
+        ListBooksMenuItem menuItem = new ListBooksMenuItem(library);
 
         assertEquals("List Books", menuItem.option());
+    }
+
+    @Test
+    public void shouldReturnTheListBooks() {
+        Library library = mock(Library.class);
+        ListBooksMenuItem menuItem = new ListBooksMenuItem(library);
+
+        menuItem.execute();
+
+        verify(library, times(1)).listBooks();
     }
 }
