ad3a212320c0aed0cc142e9fe776918cedb34a61
maharjun <maharjun@thoughtworks.com>
2015-09-25 00:16:42 +0530

    Return book menu item should take the input from the user

--- src/com/twu/biblioteca/ReturnBookMenuItem.java
+++ src/com/twu/biblioteca/ReturnBookMenuItem.java
@@ -3,7 +3,17 @@ package com.twu.biblioteca;
 
 public class ReturnBookMenuItem {
 
+    private UserInput userInput;
+
+    public ReturnBookMenuItem(UserInput userInput) {
+        this.userInput = userInput;
+    }
+
     public String option() {
         return "Return Book";
+    }
+
+    public void execute() {
+        String bookName = userInput.getInput();
     }
 }
--- test/com/twu/biblioteca/ReturnBookMenuItemTest.java
+++ test/com/twu/biblioteca/ReturnBookMenuItemTest.java
@@ -3,12 +3,28 @@ package com.twu.biblioteca;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 public class ReturnBookMenuItemTest {
 
     @Test
     public void shouldReturnTheOption() {
-        ReturnBookMenuItem returnBookMenuItem = new ReturnBookMenuItem();
+        UserInput userInput = mock(UserInput.class);
+        ReturnBookMenuItem returnBookMenuItem = new ReturnBookMenuItem(userInput);
 
-        Assert.assertEquals("Return Book", returnBookMenuItem.option());
+        assertEquals("Return Book", returnBookMenuItem.option());
+    }
+
+    @Test
+    public void shouldTakeTheInputFromTheUser() {
+        UserInput userInput = mock(UserInput.class);
+        ReturnBookMenuItem returnBookMenuItem = new ReturnBookMenuItem(userInput);
+
+        returnBookMenuItem.execute();
+
+        verify(userInput, times(1)).getInput();
     }
 }
