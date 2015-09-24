55b589a62c6279b32897a364c37ff875feec9eca
maharjun <maharjun@thoughtworks.com>
2015-09-24 23:09:36 +0530

    List books menu item should print the list books option

--- src/com/twu/biblioteca/ListBooksMenuItem.java
+++ src/com/twu/biblioteca/ListBooksMenuItem.java
@@ -2,10 +2,12 @@ package com.twu.biblioteca;
 
 public class ListBooksMenuItem {
 
+    private Display display;
     private Library library;
 
-    public ListBooksMenuItem(Library library) {
+    public ListBooksMenuItem(Library library, Display display) {
         this.library = library;
+        this.display = display;
     }
 
     public String option() {
@@ -13,6 +15,6 @@ public class ListBooksMenuItem {
     }
 
     public void execute() {
-        library.listBooks();
+        display.print(library.listBooks());
     }
 }
--- test/com/twu/biblioteca/ListBooksMenuItemTest.java
+++ test/com/twu/biblioteca/ListBooksMenuItemTest.java
@@ -3,16 +3,15 @@ package com.twu.biblioteca;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class ListBooksMenuItemTest {
 
     @Test
     public void shouldReturnTheListBooksOption() {
         Library library = mock(Library.class);
-        ListBooksMenuItem menuItem = new ListBooksMenuItem(library);
+        Display display = mock(Display.class);
+        ListBooksMenuItem menuItem = new ListBooksMenuItem(library, display);
 
         assertEquals("List Books", menuItem.option());
     }
@@ -20,10 +19,23 @@ public class ListBooksMenuItemTest {
     @Test
     public void shouldReturnTheListBooks() {
         Library library = mock(Library.class);
-        ListBooksMenuItem menuItem = new ListBooksMenuItem(library);
+        Display display = mock(Display.class);
+        ListBooksMenuItem menuItem = new ListBooksMenuItem(library, display);
 
         menuItem.execute();
 
         verify(library, times(1)).listBooks();
+    }
+
+    @Test
+    public void shouldPrintTheListBooks() {
+        Library library = mock(Library.class);
+        Display display = mock(Display.class);
+        ListBooksMenuItem menuItem = new ListBooksMenuItem(library,display);
+
+        when(library.listBooks()).thenReturn("Maharjun");
+        menuItem.execute();
+
+        verify(display, times(1)).print(library.listBooks());
     }
 }
