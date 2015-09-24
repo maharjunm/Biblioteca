a6f975f2eed152a321b811c99eb08757d209d374
maharjun <maharjun@thoughtworks.com>
2015-09-24 23:34:50 +0530

    List movies menu item should return the list movies

--- src/com/twu/biblioteca/ListMoviesMenuItem.java
+++ src/com/twu/biblioteca/ListMoviesMenuItem.java
@@ -1,7 +1,18 @@
 package com.twu.biblioteca;
 
 public class ListMoviesMenuItem {
+
+    private Library library;
+
+    public ListMoviesMenuItem(Library library) {
+        this.library = library;
+    }
+
     public String option() {
         return "List Movies";
+    }
+
+    public void execute() {
+        library.listMovies();
     }
 }
--- test/com/twu/biblioteca/ListMoviesMenuItemTest.java
+++ test/com/twu/biblioteca/ListMoviesMenuItemTest.java
@@ -4,13 +4,29 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class ListMoviesMenuItemTest {
 
     @Test
     public void shouldReturnTheOption() {
-        ListMoviesMenuItem menuItem = new ListMoviesMenuItem();
+        Library library = mock(Library.class);
+        ListMoviesMenuItem menuItem = new ListMoviesMenuItem(library);
 
         assertEquals("List Movies", menuItem.option());
     }
+
+    @Test
+    public void shouldReturnTheListMovies() {
+        Library library = mock(Library.class);
+        ListMoviesMenuItem menuItem = new ListMoviesMenuItem(library);
+
+        menuItem.execute();
+
+        verify(library, times(1)).listMovies();
+    }
+
+
 }
